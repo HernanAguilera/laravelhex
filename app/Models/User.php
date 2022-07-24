71586fa61fb4f34a0178c9b2b94f590d5e74fbf2
Hernan Aguilera <haguilera@protonmail.com>
2022-07-24 00:26:50 -0500

    Basic testing auth functionalities

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,27 +8,35 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use H34\Models\User as DomainUser;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
+    
+    public function __construct(array $attributes = [])
+    {
+        $this->fillable = array_merge($this->fillable, DomainUser::getFields());
+        $this->hidden = array_merge($this->hidden, DomainUser::getHiddenFields());
+        parent::__construct($attributes);
+    }
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = DomainUser::getFields();
+    protected $fillable = [];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = array_merge([
+    protected $hidden = [
         'password',
         'remember_token',
-    ], DomainUser::getHiddenFields());
+    ];
 
     /**
      * The attributes that should be cast.
@@ -38,4 +46,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
---
+++ tests/Feature/Auth/ResetPasswordControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class ResetPasswordControllerTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    protected $url = '/api/auth/reset-password';
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_reset_password_with_existent_user_and_correct_data()
+    {
+        $password = $this->faker->regexify('[A-Za-z0-9!"#$%&\'()*+,./:;<=>?@\^_`{|}~-]{6,100}');
+        $user = User::factory()->create([
+            'password' => Hash::make($password)
+        ]);
+
+        $response = $this->post($this->url, ['email' => $user->email], [
+            'Accept' => 'application/json'
+        ]);
+
+        $content = json_decode($response->getContent(), true);
+
+        dd($content);
+
+        $response->assertStatus(200);
+    }
+}
